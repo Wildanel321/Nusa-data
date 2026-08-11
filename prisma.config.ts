f575f7ffbc7ca34379d9f8f924e4b99bc452f69c
Wildan Nur Awaludin <wildanelgato@gmail.com>
2026-08-11 08:45:48 +0700

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,9 @@
 import "dotenv/config";
-import { defineConfig, env } from "prisma/config";
+import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   datasource: {
-    url: env("DATABASE_URL"),
+    url: process.env.DATABASE_URL || "postgresql://postgres:postgres@localhost:5432/nusadata?schema=public",
   },
   migrations: {
     seed: "ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed.ts",
